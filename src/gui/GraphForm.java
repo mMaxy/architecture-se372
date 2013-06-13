87b200a8c4878d6a3ce1d70097f7077ac5a9ce03
Ilya <i.k.zubarev@gmail.com>
2013-06-13 22:03:58 +0400

    Дорогой дневник...

---
+++ src/gui/GraphForm.java
@@ -0,0 +1,11 @@
+package gui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dell
+ * Date: 13.06.13
+ * Time: 11:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class GraphForm {
+}
